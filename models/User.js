2077354eff5faf97fde3c8c99fb84c13f1625a51
cezzarCz <erickcezar07@gmail.com>
2025-04-16 17:23:34 -0300

    Implementação do modelo User com métodos de criação e busca de users no BD

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// Definição de modelo de Users
+const pool = require('../config/db');
+
+// Definindo classe User
+class User {
+    static async create({ username, email, password }) { // Método responsavel por criar users no DB.
+        const [result] = await pool.execute(
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+            [username, email, password]
+        );
+        return result;
+    }
+
+
+    static async findByEmail(email) { // Método para consulta de users via email
+        const [rows] = await pool.execute(
+            'SELECT * FROM users WHERE email = ?',
+            [email]
+        );
+        return rows[0];
+    }
+
+}
+
+module.exports = User;
