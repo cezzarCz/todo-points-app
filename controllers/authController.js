c94d242f67d36badf244c737c1254315d48c159b
cezzarCz <erickcezar07@gmail.com>
2025-04-29 04:58:55 -0300

    Identação basica

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,44 +3,44 @@
 
 const bcrypt = require('bcrypt'); // Criptografia da senha
 const jwt = require('jsonwebtoken'); // Tokens de autenticação
-const User = require('../models/User'); 
+const User = require('../models/User');
 require('dotenv').config();
 
 exports.register = async (req, res) => {
-    try{
+    try {
         // Extraindo dados enviados no corpo da requisição
-        const {username, email, password} = req.body;
-        
+        const { username, email, password } = req.body;
+
         // Validando se os todos os campos estão presentes
         if (!username || !email || !password) {
-            return res.status(400).json({error: 'Todos os campos são obrigatórios.'}); // Codigo 400 = Bad Request
+            return res.status(400).json({ error: 'Todos os campos são obrigatórios.' }); // Codigo 400 = Bad Request
         }
 
         // Valindo se o user ja existe no BD
         const existingUser = await User.findByEmail(email);
-        if (existingUser){
-            return res.status(409).json({error: 'Email já cadastrado.'}); // Codigo 409 = Conflict
+        if (existingUser) {
+            return res.status(409).json({ error: 'Email já cadastrado.' }); // Codigo 409 = Conflict
         }
 
         // Criptografando senha do user
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // Utilizando método do modelo User para criar o User no BD
-        const newUser = await User.create({username, email, password: hashedPassword});
+        const newUser = await User.create({ username, email, password: hashedPassword });
 
         // Geração de token JWT 
         const tokenJwt = jwt.sign(
-            {userId: newUser.insertId, email},
+            { userId: newUser.insertId, email },
             process.env.JWT_SECRET,
-            {expiresIn: '1h'}
+            { expiresIn: '1h' }
         );
 
         // Retornando resposta ao cliente
-        res.status(201).json({message: 'Usuário registrado com sucesso.', tokenJwt}); // Codigo 201 = Created (Sucesso)
-    } catch (error){
+        res.status(201).json({ message: 'Usuário registrado com sucesso.', tokenJwt }); // Codigo 201 = Created (Sucesso)
+    } catch (error) {
         console.error('Erro no registro do usuário, tente novamente.', error);
-        res.status(500).json({error: 'Erro interno no servidor.'}) // Codigo 500 = Internal Server Error (Deu ruim)
-    } 
+        res.status(500).json({ error: 'Erro interno no servidor.' }) // Codigo 500 = Internal Server Error (Deu ruim)
+    }
 };
 
 
@@ -48,7 +48,7 @@ exports.login = async (req, res) => {
     try {
         // Extraindo dados da requisição
         const { email, password } = req.body;
-        
+
         // Validar se os campos estão presentes
         if (!email || !password) {
             return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
@@ -57,7 +57,7 @@ exports.login = async (req, res) => {
         // Verificando se o usuário existe no BD
         const user = await User.findByEmail(email);
         if (!user) {
-            return res.status(404).json({ error: 'Usuario não encontrado.' }); 
+            return res.status(404).json({ error: 'Usuario não encontrado.' });
         }
 
         // Verificando se a senha está correta
@@ -70,7 +70,7 @@ exports.login = async (req, res) => {
 
         // Gerando token JWT
         const tokenJwt = jwt.sign(
-            { userId: user.id , email: user.email},
+            { userId: user.id, email: user.email },
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         );
@@ -78,8 +78,9 @@ exports.login = async (req, res) => {
         // Retorna resposta ao cliente
         res.status(200).json({ message: 'Login bem-sucedido.', tokenJwt });
 
-    }  catch (error) {
+    } catch (error) {
         console.error('Erro no login do usuário, tente novamente.', error);
-        res.status(500).json({ error: 'Erro interno no servidor.' }); 
+        res.status(500).json({ error: 'Erro interno no servidor.' });
     }
 };
+
